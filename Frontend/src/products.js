bc0641212cb21a99b54f463f9a49976b4b733cd6
akeel0078 <110937834+akeel0078@users.noreply.github.com>
2024-08-06 21:10:34 +0530

    Add files via upload

---
+++ Frontend/src/products.js
@@ -0,0 +1,68 @@
+import product1 from "./assets/samsung.png";
+import product2 from "./assets/iphone.png";
+import product3 from "./assets/vivo.png";
+import product4 from "./assets/motorolla.png";
+import product5 from "./assets/oppo.png";
+import product6 from "./assets/huawei.png";
+import product7 from "./assets/oneplus.png";
+import product8 from "./assets/redmi.png";
+import product9 from "./assets/honor.png";
+
+export const PRODUCTS =[
+{
+    id:1,
+    productName:"Samsung",
+    price:1000.00,
+    productImage:product1,
+},
+{
+    id:2,
+    productName:"IPhone",
+    price:999.00,
+    productImage:product2,
+},
+{
+    id:3,
+    productName:"Vivo",
+    price:888.00,
+    productImage:product3,
+},
+{
+    id:4,
+    productName:"Motorolla",
+    price:499.00,
+    productImage:product4,
+},
+{
+    id:5,
+    productName:"Oppo",
+    price:699.00,
+    productImage:product5,
+},
+{
+    id:6,
+    productName:"Huawei",
+    price:899.00,
+    productImage:product6,
+},
+{
+    id:7,
+    productName:"Oneplus",
+    price:800.00,
+    productImage:product7,
+},
+{
+    id:8,
+    productName:"Redmi",
+    price:799.00,
+    productImage:product8,
+},
+{
+    id:9,
+    productName:"Honor",
+    price:599.00,
+    productImage:product9,
+},
+
+
+];
